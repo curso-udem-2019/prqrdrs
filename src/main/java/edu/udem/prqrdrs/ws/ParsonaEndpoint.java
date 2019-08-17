b4e5eaef56ae758d18f9aa93c86ac84d2a15abfb
jpramirez <China3010>
2019-08-17 06:53:58 -0500

    Fix all issues

--- src/main/java/edu/udem/prqrdrs/ws/ParsonaEndpoint.java
+++ src/main/java/edu/udem/prqrdrs/ws/ParsonaEndpoint.java
@@ -1,13 +1,22 @@
 package edu.udem.prqrdrs.ws;
 
+import edu.udem.prqrdrs.dto.PersonaDto;
+import edu.udem.prqrdrs.entities.PersonaEntity;
 import edu.udem.prqrdrs.service.PersonaService;
+import edu.udem.prqrdrs.ws.soap.GetPersonaRequest;
+import edu.udem.prqrdrs.ws.soap.GetPersonaResponse;
+import edu.udem.prqrdrs.ws.soap.Persona;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class ParsonaEndpoint {
 
-    private static final String NAMESPACE_URI = "http://www.baeldung.com/springsoap/gen";
+    private static final String NAMESPACE_URI = "http://udem.edu/prqrdrs/ws/soap";
 
     PersonaService personService = null;
 
@@ -17,12 +26,19 @@ public class ParsonaEndpoint {
     }
 
 
-//    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
-//    @ResponsePayload
-//    public GetCountryResponse getPersona(@RequestPayload GetPerson request) {
-//        GetCountryResponse response = new GetCountryResponse();
-//        response.setCountry(countryRepository.findCountry(request.getName()));
-//
-//        return response;
-//    }
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPersonaRequest")
+    @ResponsePayload
+    public GetPersonaResponse getPersona(@RequestPayload GetPersonaRequest request) {
+        GetPersonaResponse response = new GetPersonaResponse();
+        Persona personaWS = getPersonaWS(personService.getUserById(request.getId()));
+        response.setPersona(personaWS);
+        return response;
+    }
+
+    private Persona getPersonaWS(PersonaDto personadto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Persona personaWS = modelMapper.map(personadto, Persona.class);
+        return personaWS;
+    }
+
 }
