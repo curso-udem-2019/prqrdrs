e6c736afe94f8e90c610406a6b823625bd05c458
jpramirez <China3010>
2019-08-18 22:54:26 -0500

    Added weather code

---
+++ src/main/java/edu/udem/prqrdrs/controller/WeatherController.java
@@ -0,0 +1,49 @@
+package edu.udem.prqrdrs.controller;
+
+import edu.udem.prqrdrs.dto.PersonaDto;
+import edu.udem.prqrdrs.ws.rest.WeatherRoot;
+import edu.udem.prqrdrs.ws.service.WeatherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("weather")
+public class WeatherController {
+
+    WeatherService weatherService = null;
+
+    @Autowired
+    WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @ApiOperation(
+            value = "Retorna el clima dependiendo de la locación y el país ",
+            response = String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna los datos del clima JSON"),})
+    @RequestMapping(value = "/findByZipAndCountry/{zip}/{country}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> findByZipAndCountry(@ApiParam(value = "Zip of the location", required = true) @PathVariable("zip") String zip,
+                                                 @ApiParam(value = "Country of the location", required = true) @PathVariable("country") String country
+    ) {
+        WeatherRoot wea = null;
+        if (!zip.isEmpty() &&  !country.isEmpty()) {
+            wea = weatherService.getWeatherBy(zip, country);
+            if (wea != null) {
+                return new ResponseEntity<>(wea, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("No se encontro info", HttpStatus.NOT_FOUND);
+            }
+        }
+        return new ResponseEntity<>(new PersonaDto(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/edu/udem/prqrdrs/ws/service/WeatherService.java
@@ -0,0 +1,30 @@
+package edu.udem.prqrdrs.ws.service;
+
+import edu.udem.prqrdrs.ws.rest.WeatherRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class WeatherService {
+    private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
+    private final static String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    private final static String API_KEY = "fcc913a1c9d0c15f66b957df0f70de37";
+
+    public WeatherRoot getWeatherBy(String zip, String country) {
+        RestTemplate restTemplate = new RestTemplate();
+        String urlRestWS = String.format("%s?zip=%s,%s&APPID=%s",
+                WEATHER_URL,
+                zip,
+                country,
+                API_KEY);
+        WeatherRoot weatherRoot = restTemplate.getForObject(urlRestWS, WeatherRoot.class);
+        log.info(weatherRoot.toString());
+
+        return weatherRoot;
+    }
+
+
+}
