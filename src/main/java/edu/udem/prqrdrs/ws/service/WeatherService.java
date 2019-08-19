4134e8ca08b52a91ab0e5a58c217efb19aea1d65
jpramirez <China3010>
2019-08-19 09:46:19 -0500

    Update WIP weather

--- src/main/java/edu/udem/prqrdrs/ws/service/WeatherService.java
+++ src/main/java/edu/udem/prqrdrs/ws/service/WeatherService.java
@@ -11,15 +11,17 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherService {
     private static final Logger log = LoggerFactory.getLogger(WeatherService.class);
     private final static String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
-    private final static String API_KEY = "YOUR KEY";
+    private final static String API_KEY = "fcc913a1c9d0c15f66b957df0f70de37";
+    private final static String UNIT = "metric";
 
     public WeatherRoot getWeatherBy(String zip, String country) {
         RestTemplate restTemplate = new RestTemplate();
-        String urlRestWS = String.format("%s?zip=%s,%s&APPID=%s",
+        String urlRestWS = String.format("%s?zip=%s,%s&APPID=%s&units=%s",
                 WEATHER_URL,
                 zip,
                 country,
-                API_KEY);
+                API_KEY,
+                UNIT);
         WeatherRoot weatherRoot = restTemplate.getForObject(urlRestWS, WeatherRoot.class);
         log.info(weatherRoot.toString());
 
